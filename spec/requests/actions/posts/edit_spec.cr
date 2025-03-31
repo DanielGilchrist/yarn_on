41c7a2e6e1aa9e77dcb21015a47061ed27fe52bd
Daniel Gilchrist <danniieelg@gmail.com>
2025-03-31 21:18:49 +0100

    Fix security issue with edit

---
+++ spec/requests/actions/posts/edit_spec.cr
@@ -0,0 +1,19 @@
+require "../../../spec_helper"
+
+describe Posts::Edit do
+  it "Shows editing a post" do
+    user = UserFactory.create
+    post = PostFactory.create(&.author_id(user.id))
+    response = BrowserClient.auth_exec(user, Posts::Edit.with(post))
+    response.status_code.should eq(200)
+    response.body.should contain(post.title)
+    response.body.should contain(post.content)
+  end
+
+  it "Redirects if current user isn't author" do
+    user = UserFactory.create
+    post = PostFactory.create
+    response = BrowserClient.auth_exec(user, Posts::Edit.with(post))
+    response.status_code.should eq(302)
+  end
+end
--- src/actions/posts/edit.cr
+++ src/actions/posts/edit.cr
@@ -1,6 +1,12 @@
 class Posts::Edit < BrowserAction
   get "/posts/:post_id/edit" do
     post = PostQuery.find(post_id)
+
+    if post.author_id != current_user.id
+      flash.failure = "You are not permitted to edit this post."
+      return redirect Posts::Show.with(post)
+    end
+
     html EditPage,
       operation: SavePost.new(post, current_user: current_user),
       post: post
