100f985260bb5c7963d34f6296a70b9dde27afa7
Daniel Gilchrist <danniieelg@gmail.com>
2025-03-31 21:44:05 +0100

    Show posts in descending order

--- src/actions/posts/index.cr
+++ src/actions/posts/index.cr
@@ -1,5 +1,5 @@
 class Posts::Index < BrowserAction
   get "/posts" do
-    html IndexPage, posts: PostQuery.new.preload_author
+    html IndexPage, posts: PostQuery.new.in_descending_order.preload_author
   end
 end
--- src/queries/post_query.cr
+++ src/queries/post_query.cr
@@ -1,2 +1,5 @@
 class PostQuery < Post::BaseQuery
+  def in_descending_order
+    created_at.desc_order
+  end
 end
